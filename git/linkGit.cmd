cc08592e0717132dc40ae5a09a1c5a380cf3e392
Aaron Kor <aaron.kor@gmail.com>
2017-04-11 00:03:34 -0500

    add link cmds, update vimrc

---
+++ git/linkGit.cmd
@@ -0,0 +1,4 @@
+REM Run as Administrator
+
+MKLINK /H %HOMEPATH%\.gitconfig .gitconfig
+MKLINK /H %HOMEPATH%\.gitignore_global.txt .gitignore_global.txt
---
+++ vim/linkVim.cmd
@@ -0,0 +1,5 @@
+REM Run as Administrator
+
+MKLINK /H %HOMEPATH%\_vimrc _vimrc
+MKLINK /J %HOMEPATH%\vimfiles vimfiles
+MKLINK /J %HOMEPATH%\.vim vimfiles
