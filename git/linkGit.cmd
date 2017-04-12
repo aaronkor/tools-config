4396222626b8d05e3899a1694dbb9b7f2559ad06
Aaron Kor <aaron.kor@gmail.com>
2017-04-12 09:50:49 -0500

    fix linkGit.cmd, remove plugged plugins

--- git/linkGit.cmd
+++ git/linkGit.cmd
@@ -1,4 +1,4 @@
 REM Run as Administrator
 
 MKLINK /H %HOMEPATH%\.gitconfig .gitconfig
-MKLINK /H %HOMEPATH%\.gitignore_global.txt .gitignore_global.txt
+MKLINK /H %HOMEPATH%\gitignore_global.txt gitignore_global.txt
